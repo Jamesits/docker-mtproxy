61c5aa45485f12925c22069c629f4e4a49dd058e
James Swineson <github@public.swineson.me>
2018-12-02 16:18:52 +0800

    add support for tag

--- start-mtproxy.sh
+++ start-mtproxy.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -eu
 
+# update DC config
+echo "Updating Telegram DC config..."
+if curl -Lv https://core.telegram.org/getProxyConfig -o /etc/mtproto-proxy/proxy-multi.conf; then
+	echo "DC config updated successfully."
+else
+	echo "Unable to update DC config, using the embedded one from Docker image."
+fi
+
+APPEND_ARGS=""
+
 # gather config
 : "${THREADS:=1}"
+: "${PORT:=443}"
 INNER_IP=$(awk '/32 host/ { print f } {f=$2}' <<< "$(</proc/net/fib_trie)" | grep -v 127.0.0.1 | sed -n 1p)
 echo "Inner IP: ${INNER_IP}"
 if [ -z ${OUTER_IP+x} ]; then
@@ -17,16 +28,15 @@ if [ -z ${SECRET+x} ]; then
 	(>&2 echo "WARNING: \$SECRET set to random string \"${SECRET}\". You should set it explicitly so it would be persistent between restarts.")
 fi
 
-
-echo "Updating Telegram DC config..."
-if curl -Lv https://core.telegram.org/getProxyConfig -o /etc/mtproto-proxy/proxy-multi.conf; then
-	echo "DC config updated successfully."
+if [ -z ${TAG+x} ]; then
+	(>&2 echo "WARNING: Tag unset. Register your bot with @MTProxyBot: https://t.me/MTProxyBot")
 else
-	echo "Unable to update DC config, using the embedded one from Docker image."
+	APPEND_ARGS="${APPEND_ARGS} --proxy-tag ${TAG}"
 fi
 
-echo "Link: tg://proxy?server=${OUTER_IP}&port=443&secret=${SECRET}"
+echo "Link: tg://proxy?server=${OUTER_IP}&port=${PORT}&secret=${SECRET}"
+echo "Link: https://t.me/proxy?server=${OUTER_IP}&port=${PORT}&secret=${SECRET}"
 
 echo "Starting..."
-exec mtproto-proxy -6 -u nobody -p 8888 -H 443 -S "${SECRET}" --aes-pwd /etc/mtproto-proxy/proxy-secret /etc/mtproto-proxy/proxy-multi.conf -M "${THREADS}" --nat-info ${INNER_IP}:${OUTER_IP}
+exec mtproto-proxy -6 -u nobody -p 8888 -H 443 -S "${SECRET}" --aes-pwd /etc/mtproto-proxy/proxy-secret /etc/mtproto-proxy/proxy-multi.conf -M "${THREADS}" --nat-info ${INNER_IP}:${OUTER_IP} ${APPEND_ARGS}
 
